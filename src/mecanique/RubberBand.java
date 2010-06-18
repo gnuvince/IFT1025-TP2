a370663cf471f994f2e334f9e7a3f90acd3085e4
Vincent Foley <vfoley@gmail.com>
2010-06-17 21:00:29 -0400

    Removed positions

--- src/mecanique/RubberBand.java
+++ src/mecanique/RubberBand.java
@@ -11,61 +11,28 @@ public class RubberBand implements Drawable, Force {
     private static final double LENGTH = 100.0;
     private static final double RIGOR = 0.1;
     
-    private PhysicalSystem system = null;
-    
-    private Point2D left_point = null;
-    private Point2D right_point = null;
     private PhysicalObject left_object = null;
     private PhysicalObject right_object = null;
     
-    public RubberBand(Point2D l, Point2D r, PhysicalSystem system) {
-        this.system = system;
-        setLeft_point(new Point2D.Double(l.getX(), l.getY()));
-        setRight_point(new Point2D.Double(r.getX(), r.getY()));
+    public RubberBand(PhysicalObject l, PhysicalObject r) {
+        left_object = l;
+        right_object = r;
     }
        
     @Override
     public void draw(Graphics2D G) {
         Graphics2D g2 = (Graphics2D) G.create();
         g2.drawLine(
-                (int) getLeft_point().getX(),
-                (int) getLeft_point().getY(),
-                (int) getRight_point().getX(),
-                (int) getRight_point().getY());
+                (int) left_object.getPosition().getX(),
+                (int) left_object.getPosition().getY(),
+                (int) right_object.getPosition().getX(),
+                (int) right_object.getPosition().getY()
+               );
     }
 
     @Override
     public Point2D getAcceleration(PhysicalObject object) {
         return new Point2D.Double();
-    }
-    
-    
-    public void setLeft_point(Point2D left_point) {
-        this.left_point = left_point;
-        this.left_object = system.closestObject(
-                left_point.getX(), left_point.getY());
-    }
-
-    public Point2D getLeft_point() {
-        return left_point;
-    }
-
-
-    public void setRight_point(Point2D right_point) {
-        this.right_point = right_point;
-        this.right_object = system.closestObject(
-                right_point.getX(), right_point.getY());
-    }
-
-    public Point2D getRight_point() {
-        return right_point;
-    }
-    
-    private void fixPoints() {
-        if (left_object != null && right_object != null) {
-            setLeft_point(left_object.getPosition());
-            setRight_point(right_object.getPosition());
-        }
     }
 
 
@@ -74,8 +41,7 @@ public class RubberBand implements Drawable, Force {
         private SystemDisplay panneau;
         private RubberBand rb;
 
-        protected Placement(SystemDisplay panneau)
-        {
+        protected Placement(SystemDisplay panneau) {
             this.panneau = panneau;
             rb = null;
         }
@@ -92,6 +58,11 @@ public class RubberBand implements Drawable, Force {
             }
         }
         
+        private PhysicalObject getClosestObject(Point2D position) {
+            PhysicalObject po = panneau.getSystem().closestObject(position.getX(), position.getY());
+            return po;
+        }
+        
         @Override
         public void mouseClicked(MouseEvent e) {
             // verifier le bouton du souris
@@ -99,15 +70,13 @@ public class RubberBand implements Drawable, Force {
                 clear();
             } 
             else {
+                PhysicalObject po = getClosestObject(e.getPoint());
                 if (rb == null) {
-                    rb = new RubberBand(
-                            e.getPoint(),
-                            e.getPoint(),
-                            panneau.getSystem());
+                    rb = new RubberBand(po, po);
                     panneau.add(rb);
                 } 
                 else {
-                    rb.setRight_point(e.getPoint());
+                    rb.right_object = po;
                     
                     // Garder l'élastique seulement s'il est attaché à deux objets
                     // différents.
@@ -115,9 +84,6 @@ public class RubberBand implements Drawable, Force {
                         rb.right_object == null ||
                         rb.left_object == rb.right_object) {
                         clear();
-                    }
-                    else {
-                        rb.fixPoints();
                     }
                     
                     rb = null;
@@ -129,7 +95,8 @@ public class RubberBand implements Drawable, Force {
         @Override
         public void mouseMoved(MouseEvent e) {
             if (rb != null) {
-                rb.setRight_point(e.getPoint());
+                PhysicalObject po = getClosestObject(e.getPoint());
+                rb.right_object = po;
                 panneau.repaint();
             }
         }
